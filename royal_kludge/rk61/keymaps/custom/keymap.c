72300fbb3f0b523e01714b8666dfd2545f86e201
Samantha <trackmenot10@gmail.com>
2025-02-14 20:01:37 -0800

    Swap Alt/Win, Caps/Alt

--- royal_kludge/rk61/keymaps/custom/keymap.c
+++ royal_kludge/rk61/keymaps/custom/keymap.c
@@ -14,11 +14,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_TAB,     KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,  KC_LBRC,  KC_RBRC,  KC_BSLS,
      KC_ESC,     KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,  KC_SCLN,  KC_QUOT,   KC_ENT,
     KC_LSFT,     KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,  KC_COMM,   KC_DOT, KC_SLASH,  KC_RSFT,KC_NO,KC_NO,
-    KC_LCTL,      MO(_VIM),     KC_LALT,      KC_SPC,                                               KC_BSPC,     KC_CAPS,    MO(_VIM),    MO(_MEDIA)
+    KC_LCTL,      MO(_VIM),     KC_GUI,      KC_SPC,                                               KC_BSPC,     KC_ALT,    MO(_VIM),    MO(_MEDIA)
 ),
 
 [_VIM] = LAYOUT_all(
-    KC_GRV,     KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,   KC_F10,   KC_F11,   KC_F12,  KC_DEL,_______,
+     KC_GRV,    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,   KC_F10,   KC_F11,   KC_F12,  KC_DEL,_______,
     _______,  _______,    KC_UP,  _______,  _______,  _______,  KC_PSCR,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
     _______,  KC_LEFT,  KC_DOWN,  KC_RGHT,  _______,  _______,  KC_LEFT,  KC_DOWN,    KC_UP,  KC_RGHT,  KC_LPRN,  KC_RPRN,  _______,
     _______,  _______,  _______,  _______,  _______,  _______,  KC_HOME,  KC_PGDN,  KC_PGUP,   KC_END,  _______,  _______,_______,_______,
