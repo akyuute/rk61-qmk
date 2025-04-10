64a8fcfa62e2909a316cc88490e8b3584aef4b89
Samantha <trackmenot10@gmail.com>
2025-04-09 21:49:22 -0700

    Reinstate RH Fn1; Fix useless Alt+Tab combo key; Add Fn1 Tabs below homerow, MB4,5

--- royal_kludge/rk61/keymaps/vimwin/keymap.c
+++ royal_kludge/rk61/keymaps/vimwin/keymap.c
@@ -14,23 +14,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_TAB,     KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,  KC_LBRC,  KC_RBRC,  KC_TAB,
      KC_ESC,     KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,  KC_SCLN,  KC_QUOT,  KC_ENTER,
     KC_LSFT,     KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,  KC_COMM,   KC_DOT, KC_SLASH,  KC_RSFT,KC_NO,KC_NO,
-    KC_LGUI,     KC_LCTL,     MO(_VIM),     KC_SPC,                                                KC_BSPC,     KC_RALT,     QK_REP,     MO(_MEDIA)
+    KC_LGUI,     KC_LCTL,     MO(_VIM),     KC_SPC,                                                KC_BSPC,     KC_RALT,     MO(_VIM),     MO(_MEDIA)
 ),
 
 [_VIM] = LAYOUT_all(
      KC_GRV,    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,   KC_F10,   KC_F11,   KC_F12,  KC_DEL,_______,
-LALT(KC_TAB), _______,    KC_UP,  KC_PERC,  KC_CIRC,  KC_AMPR,  KC_HOME,  KC_PGDN,  KC_PGUP,   KC_END,   KC_GRV,  DM_REC1, DM_REC2, LSA(KC_TAB),
-    KC_LALT,  KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_UNDS,  KC_ASTR,  KC_LEFT,  KC_DOWN,    KC_UP,  KC_RGHT,  KC_LPRN,  KC_RPRN,  KC_UP,
-    _______,  _______,  _______,    KC_F2,    KC_F5,  KC_PSCR,  KC_LPRN,  KC_RPRN, KC_MINUS, KC_EQUAL,  KC_BSLS,  _______,_______,_______,
-    _______,      _______,      _______,      KC_ENTER, 	                                         KC_DEL,     KC_APP,     QK_AREP,     MO(_SPEC)
+    _______,  KC_PSCR,    KC_UP,  KC_PERC,  KC_CIRC,  KC_AMPR,  KC_HOME,  KC_PGDN,  KC_PGUP,   KC_END,   KC_GRV,  DM_REC1,  DM_REC2,  _______,
+    KC_BSPC,  KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_ASTR,  KC_UNDS,  KC_LEFT,  KC_DOWN,    KC_UP,  KC_RGHT,   QK_REP,  QK_AREP,  KC_RGUI,
+    _______,  KC_LALT,   KC_TAB,    KC_F2,    KC_F5,  S(KC_TAB),KC_LPRN,  KC_RPRN, KC_MINUS, KC_EQUAL,  KC_BSLS,  _______,_______,_______,
+    _______,      _______,      _______,      KC_ENTER, 	                                         KC_DEL,     KC_APP,     _______,     MO(_SPEC)
 ),
 
 [_MEDIA] = LAYOUT_all(
     _______,  KC_BRID,  KC_BRIU,  _______,  _______,  _______,  KC_MSTP,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  _______,_______,
-    _______,  _______,    MS_UP,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  DM_PLY1, DM_PLY2, _______,
-    KC_CAPS,  MS_LEFT,  MS_DOWN,  MS_RGHT,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_RGUI,
-    _______,  MS_WHLL,  MS_WHLU,  MS_WHLD,  MS_WHLR,  _______,  _______,  _______,  _______,  MS_ACL1,  MS_ACL0,  MS_ACL2,_______,_______,
-    _______,      _______,      MO(_SPEC),     KC_MPLY,                                             MS_BTN1,     MS_BTN2,      KC_BTN3,     _______
+    _______,  _______,    MS_UP,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  DM_PLY1,  DM_PLY2,  _______,
+    KC_CAPS,  MS_LEFT,  MS_DOWN,  MS_RGHT,  _______,  _______,  _______,  _______,  _______,  _______,  MS_ACL0,  MS_ACL1,  MS_ACL2,
+    _______,  MS_WHLL,  MS_WHLU,  MS_WHLD,  MS_WHLR,  _______,  _______,  _______,  _______,  MS_BTN4,  MS_BTN3,  MS_BTN5,_______,_______,
+    _______,      _______,      MO(_SPEC),     KC_MPLY,                                             MS_BTN1,     MS_BTN2,      MO(_SPEC),     _______
 ),
 
 [_SPEC] = LAYOUT_all(
@@ -41,7 +41,7 @@ LALT(KC_TAB), _______,    KC_UP,  KC_PERC,  KC_CIRC,  KC_AMPR,  KC_HOME,  KC_PGD
     _______,      _______,      _______,      _______, 	                                           _______,      _______,      _______,     _______
 */
 
-    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  QK_BOOT,_______,
+    QK_BOOT,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_PWR,_______,
     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
     _______,  _______,  KC_SLEP,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_SLEP,
     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  BL_DOWN,  BL_UP,    _______,  _______,_______,_______,
